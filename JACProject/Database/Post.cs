ed7d3eb03f14b1e104c6fd54868cdd65001c54fb
James Wong-Tung <2021219@techtorium.ac.nz>
2019-11-22 09:36:00 +1300

    Webfront Merged

--- JACProject/Database/Post.cs
+++ JACProject/Database/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace JACProject.Database
     public class Post
     {
         public int id { get; set; }
+        [DataType(DataType.MultilineText)]
         public string text { get; set; }
         public DateTime timeCreated { get; set; }
         public Account creator { get; set; }
